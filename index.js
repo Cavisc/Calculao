0e8fb26aa9f225590eedd82ff140c93e1b9876df
Carlos Vinícius <cavisc.icm@gmail.com>
2022-10-14 16:26:12 -0300

    Adicionado efeito digitação no título

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ import Calculao from "./Calculao/Calculao.js";
 
 const stage = new Stage({ costumeNumber: 1 });
 
+const titulo = document.querySelector('#titulo');
+const texto = 'App do Calculão';
+let i = 0;
+const digita = () => {
+  titulo.textContent = texto.slice(0, i++);
+}
+
+setInterval(digita, 150);
+
 const sprites = {
   Calculao: new Calculao({
     x: 0,
